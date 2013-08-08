e59463157b337f32abeca8a5bb48d9c671e01880
Gabe Fierro <gtfierro225@gmail.com>
2013-08-08 12:57:55 -0700

    add linear regression analysis of groups to find signature

--- cv/signature.py
+++ cv/signature.py
@@ -3,13 +3,16 @@
 Toy library for extracting signature from an image
 """
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import numpy as np
 from skimage.data import imread
 from skimage.color import rgb2gray
 from skimage.measure import find_contours
 from scipy.spatial import distance
+from scipy import stats
 from sklearn.cluster import DBSCAN
 from itertools import cycle
+from collections import defaultdict
 import math
 import os
 import sys
@@ -85,6 +88,53 @@ def run_dbscan(contours, eps=400, min_samples=3):
                 plt.plot(point[0], point[1], color=color, marker='o')
     return zip(labels, pointarray)
 
+def extract_signature(contours, pointlabels):
+    """
+    Given the [contours] from compute_contours and the list of (label, point) tuples
+    from run_dbscan, finds the most horizontal group of points and draws
+    a bounding box around the corresponding group of centroids.
+    """
+    clusters = defaultdict(list)
+    # cluster points by their group
+    for k,v in pointlabels:
+        if k != -1: # -1 is not a group, so we skip
+            clusters[k].append(v)
+    if not clusters:
+        print 'no signature found'
+        return
+    # compute linear regression for each group
+    fits = {}
+    for label, cluster in clusters.iteritems():
+        array = np.array(cluster)
+        xi = array[:, 0]
+        y = array[:, 1]
+        slope, intercept, r_value, p_value, std_err = stats.linregress(xi, y)
+        # want to minimize slope and error, so we add
+        # their absolute values and take the min
+        fits[label] = abs(slope) + abs(std_err)
+    bestfit_label = min(fits, key=lambda x: fits[x])
+    # get indices into the contours list from the list of points
+    tmp_points = [tuple(x[1]) for x in pointlabels]
+    indices = [tmp_points.index(tuple(x)) for x in clusters[bestfit_label]]
+    minx = miny = float('inf')
+    maxx = maxy = float('-inf')
+    for index in indices:
+        contour = contours[index]
+        minx = min(minx, min(contour, key=lambda x: x[1])[1])
+        miny = min(miny, min(contour, key=lambda x: x[0])[0])
+        maxx = max(maxx, max(contour, key=lambda x: x[1])[1])
+        maxy = max(maxy, max(contour, key=lambda x: x[0])[0])
+    maxx += 10
+    maxy += 10
+    minx -= 10
+    miny -= 10
+    x = (minx, maxx, maxx, minx, minx)
+    y = (miny, miny, maxy, maxy, miny)
+    plt.plot(x, y, '-b', linewidth=2)
+    plt.gray()
+    plt.imshow(img)
+    plt.savefig('out.png')
+
 def iterate(filename, lengths, epsilons, min_samples):
     """
     Given 3 lists of values for [length], [eps] and [min_samples],
@@ -110,6 +160,7 @@ if __name__=='__main__':
     img = get_image(imagepath)
     contours, lengths = compute_contours(img)
     points = run_dbscan(contours)
+    extract_signature(contours, points)
 
     plt.gray()
     plt.imshow(img)
